25a2cb8da9aa93866aaa932f69ac0698d37e0c57
alwinantony <alwinantony4@yahoo.com>
2018-06-06 14:19:23 -0700

    Moved server.post back up, didn't seem to cause the "waiting for bot to be ready" issue. That happens sometiems when codes are updated.

--- app.js
+++ app.js
@@ -34,10 +34,7 @@ var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
 // Create your bot with a function to receive messages from the user
-//var bot = new builder.UniversalBot(connector);
-var bot = new builder.UniversalBot(connector, function (session, args) {
-    session.send('You reached the default message handler. You said \'%s\'.', session.message.text);
-});
+var bot = new builder.UniversalBot(connector);
 bot.set('storage', tableStorage);
 
 /*
